e8671559af9ae0fe5cf435dbe094d4c9c5f15c71
penguinruby <81369983+penguinruby@users.noreply.github.com>
2024-06-22 23:36:39 +0200

    Update asking.py

--- asking.py
+++ asking.py
@@ -1,14 +1,4 @@
-''' 
-Q1.測試太多次會被網站擋下(大約10來次以上，也有可能因為我每改依)。會跳到一個頁面去按"我不是機器人"。不幸的話，還會跳九宮格。請問有解決方式嗎？
-Q2.用selenium自動化測試，為何還會被擋？所以用selenium也跟用request 是一樣的嗎
-Q3.這個頁面的網址為https://www.mueller.de/sale/alle-produkte/，下一頁就是很規律的https://www.mueller.de/sale/alle-produkte/?p="數字"。
-但是如果這次跑的時候全部只有28頁，把迴圈設定50頁的情況下，28頁之後全部都會出現在28頁。
-也就是說，這樣28頁的東西就會抓好幾次。如果只設定28頁，但下次的特價頁面超過28頁的話，就又會抓不到。
-不想用.click()下一頁的情況下，有可能解決這個問題嗎？
-外帶目前下一頁的按鈕我找到定位但不知道為何都無法跑。
 
-
-'''
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -20,66 +10,105 @@ prices = [] #特價價格
 ids = [] #商品編號
 piclinks = [] #商品圖片連結
 productLinks = []  #商品連結保存
-# pagelinks = []  #每一頁的網址
-test=0
+pagelinks = []  #每一頁的網址
+brands=[] #全品牌蒐集
+productsbrand=[] #商品品牌蒐集
+brandslowers=[] #品牌大小寫轉換
+
+
+
 
 driver=webdriver.Chrome()
 driver.get("https://www.mueller.de/sale/alle-produkte/")
 driver.implicitly_wait(10)
+wait = WebDriverWait(driver, 10)
 
-for page in range(1, 30):
-    # 獲取當前頁面的商品連結   OK
-    # for i in range(1, 21):  # 假設每頁有20個商品
-    #     try:
-    #         productLink = driver.find_element(By.XPATH, f'//*[@id="page"]/main/div[2]/div/div/div/div[2]/div[3]/div/div/a[{i}]')
-    #         productLinks.append(productLink.get_attribute('href'))
-    #     except:
-    #         print(f'商品連結 {i} 無法獲取')
-    
-    # 點擊下一頁按鈕
-    try:
-        driver.find_element(By.CLASS_NAME,"mu-button2 mu-button2--icon-only mu-pagination__navigation mu-pagination__navigation--next").click()
-        test += 1
-        print(f'已點擊第 {test} 頁')
-    except:
-        print('下一頁按鈕無法點擊，結束')
-        break
+#下一頁蒐集 OK
+total_pages =int(driver.find_element(By.XPATH, '//*[@id="page"]/main/div[2]/div/div/div/div[2]/div[3]/div/nav/div/button[4]/span').text)
+for i in range(1, total_pages):
+    pagelinks.append(f'https://www.mueller.de/sale/alle-produkte/?p={i}')
+
+
+#品牌蒐集 OK
+for i in range(1,334):
+    brand=driver.find_element(By.XPATH, f'//*[@id="page"]/main/div[2]/div/div/div/div[1]/div/ul/li[2]/div/div/ul/li[{i}]/label/div[2]/span[1]')
+    brands.append(brand.text)
+
+for i in brands:  #品牌大小寫轉換以利配對 OK
+    brandlower= i.lower()
+    brandslowers.append(brandlower)
 
 
 
-for i in range (1,61):
-#商品連結 OK
-    productLink = driver.find_element(By.XPATH, '//*[@id="page"]/main/div[2]/div/div/div/div[2]/div[3]/div/div/a['+str(i)+']')
-    productLinks.append(productLink.get_attribute('href'))
+for link in pagelinks:
+    driver.get(link)
+    driver.implicitly_wait(10)
+    # 獲取當前頁面的商品連結 OK
+    for i in range(1, 5):  
+        try:
+            productLink = driver.find_element(By.XPATH, f'//*[@id="page"]/main/div[2]/div/div/div/div[2]/div[3]/div/div/a[{i}]')
+            productLinks.append(productLink.get_attribute('href'))
+        except:
+            break
 
-#進入連結 OK
+    #進入連結 OK
 for link in productLinks:
     driver.get(link)
     driver.implicitly_wait(10)
 
- #商品圖片 OK
+    #商品圖片 OK
     piclink = driver.find_element(By.XPATH, '//*[@id="page"]/main/div[1]/div/div[1]/div[1]/div[2]/div[1]/div/img')
     piclinks.append(piclink.get_attribute('src')) 
 
-#商品名稱  OK
+    #商品名稱  OK
     productName = driver.find_element(By.XPATH, '//*[@id="page"]/main/div[1]/div/div[1]/div[2]/div[1]/h1')
-    productNames.append(productName.text)
+    productName = productName.text
+    productNames.append(productName)
 
 
-# products id OK
+#------------------------
+# 遍歷每個商品名稱---test
+    for j in productName:
+        j= j.split(" ")
+
+
+
+    for product in j:
+        matched = False
+        for brand in brands:
+            if brand in product.lower():
+                productsbrand.append(brand)
+                matched = True
+                break
+
+
+    print(productsbrand)
+#------------------------
+#end of testing 
+
+    # products id OK
     id = driver.find_element(By.XPATH, '//*[@id="page"]/main/div[1]/div/div[1]/div[2]/div[1]/div[2]')
     ids.append(id.text)
 
-#特價價格
+
+#特價價格 OK
     try:
-        price = driver.find_element(By.XPATH, '//*[@id="page"]/main/div[1]/div/div[1]/div[2]/div[3]/div/div[1]/div[1]/span[3]')   #OK
+        price = driver.find_element(By.XPATH, '//*[@id="page"]/main/div[1]/div/div[1]/div[2]/div[3]/div/div[1]/div[1]/span[3]')
         prices.append(price.text)
     except:
-        price = driver.find_element(By.XPATH, '//*[@id="page"]/main/div[1]/div/div[1]/div[2]/div[3]/div/div[1]/div[1]/span[2]')   #OK
-        prices.append(price.text)
-    else:
-        price = driver.find_element(By.XPATH, '//*[@id="page"]/main/div[1]/div/div[1]/div[2]/div[3]/div/div[1]/div/div/div[2]')  #ok
-        prices.append(price.text)
+        try:
+            price = driver.find_element(By.XPATH, '//*[@id="page"]/main/div[1]/div/div[1]/div[2]/div[3]/div/div[1]/div[1]/span[2]')
+            prices.append(price.text)
+        except:
+            price = driver.find_element(By.XPATH, '//*[@id="page"]/main/div[1]/div/div[1]/div[2]/div[3]/div/div[1]/div/div/div[2]')
+            prices.append(price.text)
+
+
+
+
+for i in prices:
+    print(i)
 
 driver.quit()
+
 
