c64561e983dc45e04f99ba4de599b3040967f95b
penguinruby <81369983+penguinruby@users.noreply.github.com>
2024-06-23 13:17:27 +0200

    Update and rename asking.py to muller.py

--- muller.py
+++ muller.py
@@ -1,9 +1,8 @@
-
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+import csv
+
 
 productNames=[] #商品名稱
 prices = [] #特價價格
@@ -14,6 +13,7 @@ pagelinks = []  #每一頁的網址
 brands=[] #全品牌蒐集
 productsbrand=[] #商品品牌蒐集
 brandslowers=[] #品牌大小寫轉換
+data =[]
 
 
 
@@ -29,26 +29,26 @@ for i in range(1, total_pages):
     pagelinks.append(f'https://www.mueller.de/sale/alle-produkte/?p={i}')
 
 
-for i in range(1, 2):  #test
-    pagelinks.append(f'https://www.mueller.de/sale/alle-produkte/?p={i}')
-
 
 #品牌蒐集 OK
 for i in range(1,334):
-    brand=driver.find_element(By.XPATH, f'//*[@id="page"]/main/div[2]/div/div/div/div[1]/div/ul/li[2]/div/div/ul/li[{i}]/label/div[2]/span[1]')
-    brands.append(brand.text)
+    try:
+        brand=driver.find_element(By.XPATH, f'//*[@id="page"]/main/div[2]/div/div/div/div[1]/div/ul/li[2]/div/div/ul/li[{i}]/label/div[2]/span[1]')
+        brands.append(brand.text)
+    except:
+        break
 
+        
 for i in brands:  #品牌大小寫轉換以利配對 OK
     brandlower= i.lower()
     brandslowers.append(brandlower)
-
 
 
 for link in pagelinks:
     driver.get(link)
     driver.implicitly_wait(10)
     # 獲取當前頁面的商品連結 OK
-    for i in range(1, 5):  
+    for i in range(1, 61):  
         try:
             productLink = driver.find_element(By.XPATH, f'//*[@id="page"]/main/div[2]/div/div/div/div[2]/div[3]/div/div/a[{i}]')
             productLinks.append(productLink.get_attribute('href'))
@@ -73,7 +73,7 @@ for link in productLinks:
         j= j.split(" ")
 
 
-# 遍歷每個商品名稱跟品牌配對  OK
+# 遍歷每個商品名稱  OK
     for product in j:
         matched = False
         for brand in brandslowers:
@@ -81,7 +81,6 @@ for link in productLinks:
                 productsbrand.append(brand)
                 matched = True
                 break
-
 
 
     # products id OK
@@ -98,13 +97,30 @@ for link in productLinks:
             price = driver.find_element(By.XPATH, '//*[@id="page"]/main/div[1]/div/div[1]/div[2]/div[3]/div/div[1]/div[1]/span[2]')
             prices.append(price.text)
         except:
-            price = driver.find_element(By.XPATH, '//*[@id="page"]/main/div[1]/div/div[1]/div[2]/div[3]/div/div[1]/div/div/div[2]')
-            prices.append(price.text)
+            try:
+                price = driver.find_element(By.XPATH, '//*[@id="page"]/main/div[1]/div/div[1]/div[2]/div[3]/div/div[1]/div/div/div[2]')
+                prices.append(price.text)
+            except:
+                prices.append("not catch")
 
 
 
+for a,b,c,d,*e in ids,productNames,productsbrand,prices,piclinks:
+    data.append({
+            'Id': a,
+            '商品名稱': b,
+            '商品品牌' : c,
+            '特價價格': d,
+            '圖片連結': e
 
-for i in prices:
-    print(i)
+    })
+
+
+
+with open('muller.csv', 'w', newline='', encoding='utf-8-sig') as file:
+
+    writer = csv.writer(file)
+    for item in data:
+        writer.writerow([item['Id'], item['商品名稱'], item['商品品牌'], item['特價價格'],item["圖片連結"]])
 
 driver.quit()
